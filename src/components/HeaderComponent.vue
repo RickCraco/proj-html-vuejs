da2f1072e9865d74cc6720ce780a171ed3af544a
RickCraco <riccardocracolici43@gmail.com>
2023-11-21 14:55:53 +0100

    prova layout statico

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,24 @@
 <template>
-    <h1>Ciao</h1>
+    <div class="container d-flex justify-content-between">
+        <div>
+            <select name="" id=""></select>
+        </div>
+        <div>
+            <ul class="d-flex list-unstyled">
+                <li class="px-3">1</li>
+                <li class="px-3">2</li>
+                <li class="px-3">3</li>
+                <li class="px-3">4</li>
+                <li class="px-3">5</li>
+                <li class="px-3">6</li>
+                <li class="px-3">7</li>
+                <li class="px-3">8</li>
+                <li class="px-3">9</li>
+                <li class="px-3">10</li>
+                <li class="px-3">11</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
