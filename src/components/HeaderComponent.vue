db9883cf3ee6a3389ea28b95383979bb454bebac
RickCraco <riccardocracolici43@gmail.com>
2023-11-21 14:50:10 +0100

    creato componente header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,7 +6,15 @@
 
 <script>
     export default {
-        
+        name : 'HeaderComponent',
+        data(){
+            return{
+
+            }
+        },
+        methods : {
+            
+        }
     }
 </script>
 
