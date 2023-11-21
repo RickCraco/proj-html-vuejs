a5dc2b26a7d41d78145ee16d59759636f06f92ee
RickCraco <riccardocracolici43@gmail.com>
2023-11-21 16:33:56 +0100

    aggiunto componente card al main component

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,14 +2,18 @@
     <div class="container mt-4 pt-4">
         <h2>Popular Development Courses</h2>
         <div class="row">
-            
+            <CardComponent/>
         </div>
     </div>
 </template>
 
 <script>
+    import CardComponent from './CardComponent.vue';
     export default {
         name: 'MainComponent',
+        components: {
+            CardComponent
+        },
         data(){
             return{
 
