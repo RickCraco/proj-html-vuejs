75b691df3e76de49b7d3e6778bac89a8696e972e
RickCraco <riccardocracolici43@gmail.com>
2023-11-21 15:47:55 +0100

    aggiunto menu dinamicamente

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,17 +5,15 @@
         </div>
         <div>
             <ul class="d-flex list-unstyled align-items-center">
-                <li class="px-3">1</li>
-                <li class="px-3">2</li>
-                <li class="px-3">3</li>
-                <li class="px-3">4</li>
-                <li class="px-3">5</li>
-                <li class="px-3">6</li>
-                <li class="px-3">7</li>
-                <li class="px-3">8</li>
-                <li class="px-3">9</li>
-                <li class="px-3">10</li>
-                <li class="px-3">11</li>
+                <li class="px-3" v-for="item in pageMenu">{{ item.name }}</li>
+            </ul>
+            <ul class="d-flex list-unstyled align-items-center">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
             </ul>
         </div>
     </div>
