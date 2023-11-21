724f525bbd7d3cf2fdfce3b60ce0837fc538e15c
RickCraco <riccardocracolici43@gmail.com>
2023-11-21 16:52:45 +0100

    aggiunte props all'interno della card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="card col" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img :src="img" class="card-img-top" alt="">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
+            <h5 class="card-title">{{ category }}</h5>
+            <p class="card-text">{{ text }}</p>
+            <p>{{ voto }}</p>
+            <span>{{ price }}</span>
         </div>
     </div>
 </template>
