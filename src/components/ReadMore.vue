ed39769adf7dd7f7396236f88ee70b0cc2cfc3ab
RickCraco <riccardocracolici43@gmail.com>
2023-11-22 10:11:45 +0100

    spostate classi scss e template dal main component al readmore componente

--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -1,5 +1,13 @@
 <template>
-    
+    <div class="d-flex justify-content-center my-bg-img align-items-center">
+        <div class="my-margin">
+            <h2>
+                Limitless learning, more possibilities
+            </h2>
+            <p>Online courses open the opportunity for learning to almost anyone, regardless of their scheduling commitments.</p>
+            <button>READ MORE</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +17,15 @@
 </script>
 
 <style lang="scss" scoped>
+    .my-bg-img{
+        background-image: url('/img/bg.png');
+        width: 100%;
+        height: 650px;
+        background-repeat: no-repeat;
+    }
 
+    .my-margin{
+        margin-right: 500px;
+        margin-bottom: 200px;
+    }
 </style>
