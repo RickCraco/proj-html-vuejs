47b6bd0012e0dc01e1c2da5c4c549595067c04e6
RickCraco <riccardocracolici43@gmail.com>
2023-11-22 11:01:20 +0100

    fix

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="my-bg-red text-white d-flex justify-content-center align-items-center my-3 p-3">
         <div>
-            <h5>Subscribe our newsLetter</h5>
+            <h5 class="m-0">Subscribe our newsLetter</h5>
             <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered <br> alteration in some form, by injected humour, or randomised words</p>
         </div>
-        <div>
-            <span>YOUR EMAIL ADDRESS</span>
+        <div class="ms-5">
+            <p>YOUR EMAIL ADDRESS</p>
             <input type="text" placeholder="Enter your E-mail">
         </div>
     </div>
