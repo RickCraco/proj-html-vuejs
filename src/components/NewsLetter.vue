1c44be4e4116ac5b5fe3f3c2049b39e4276338da
RickCraco <riccardocracolici43@gmail.com>
2023-11-22 10:56:35 +0100

    aggiunto template newsletter

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -1,5 +1,14 @@
 <template>
-
+    <div class="my-bg-red text-white d-flex justify-content-center align-items-center my-3 p-3">
+        <div>
+            <h5>Subscribe our newsLetter</h5>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered <br> alteration in some form, by injected humour, or randomised words</p>
+        </div>
+        <div>
+            <span>YOUR EMAIL ADDRESS</span>
+            <input type="text" placeholder="Enter your E-mail">
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +18,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .my-bg-red{
+        background-color: #FF4450;
+    }
 </style>
