119e93210b1da70cb55eeea617c8e29a0b69b8d4
RickCraco <riccardocracolici43@gmail.com>
2023-11-22 18:06:25 +0100

    importato componente bottone

---
+++ src/components/Button.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="mt-4 py-4">
+        <button class="btn position-relative">
+            <span>READ MORE</span>
+            <div></div>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Button'
+}
+</script>
+
+<style lang="scss" scoped>
+    button{
+        background-color: #FF4450;
+        color: white;
+        padding: 20px;
+        padding-left: 40px;
+        padding-right: 40px;
+        border-radius: 35px;
+        overflow: hidden;
+
+        >span{
+            z-index: 200;
+            position: relative;
+        }
+
+        >div{
+            height: 100%;
+            width: 0px;
+            transition: all 0.5s ease;
+            position: absolute;
+            left: 0;
+            top: 0;
+            z-index: 1;
+            background-color: rgba($color: #000000, $alpha: 0.2);
+        }
+
+        &:hover{
+            background-color: #FF4450;
+            color: white;
+            >div{
+                width: 100%;
+            }
+        }
+    }
+</style>
--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -5,19 +5,18 @@
                 Limitless learning,<br>more possibilities
             </h2>
             <p>Online courses open the opportunity for learning to almost anyone,<br>regardless of their scheduling commitments.</p>
-            <div class="mt-4 py-4">
-                <button class="btn position-relative">
-                    <span>READ MORE</span>
-                    <div></div>
-                </button>
-            </div>
+            <Button/>
         </div>
     </div>
 </template>
 
 <script>
+    import Button from './Button.vue';
     export default {
         name: 'ReadMore',
+        components: {
+            Button
+        }
     }
 </script>
 
@@ -41,40 +40,6 @@
     p{
         font-size: 18px;
         font-weight: 600;
-    }
-
-    button{
-        background-color: #FF4450;
-        color: white;
-        padding: 20px;
-        padding-left: 40px;
-        padding-right: 40px;
-        border-radius: 35px;
-        overflow: hidden;
-
-        >span{
-            z-index: 200;
-            position: relative;
-        }
-
-        >div{
-            height: 100%;
-            width: 0px;
-            transition: all 0.5s ease;
-            position: absolute;
-            left: 0;
-            top: 0;
-            z-index: 1;
-            background-color: rgba($color: #000000, $alpha: 0.2);
-        }
-
-        &:hover{
-            background-color: #FF4450;
-            color: white;
-            >div{
-                width: 100%;
-            }
-        }
     }
 
 </style>
