2684a0421464f50e7352b0261ada25dec5e85fcd
RickCraco <riccardocracolici43@gmail.com>
2023-11-22 18:02:46 +0100

    sistemato stile componente partner e readmore

--- src/components/PartnerComponent.vue
+++ src/components/PartnerComponent.vue
@@ -1,34 +1,42 @@
 <template>
-    <div class="container mt-5 d-flex">
-        <div class="border p-3 d-flex align-items-center justify-content-around mx-2">
-            <div>
-                <img src="/img/image_box_1-221x231.png" alt="">
+    <div class="container">
+        <div class="mt-5 justify-content-center row">
+            <div class="p-3 col-6">
+                <div class="border d-flex align-items-center justify-content-around p-3">
+                    <div>
+                        <img src="/img/image_box_1-221x231.png" alt="">
+                    </div>
+                    <div class="ms-3">
+                        <h3 class="my-font-size">Become an Instructor</h3>
+                        <p class="fs-5">Teach what you love. Masterstudy gives you the tools to create a course.</p>
+                        <button>START TEACHING</button>
+                    </div>
+                </div>
             </div>
-            <div class="ms-3">
-                <h3>Become an Instructor</h3>
-                <p>Teach what you love. Masterstudy gives you the tools to create a course.</p>
-                <button>START TEACHING</button>
-            </div>
-        </div>
-        <div class="border p-3 d-flex align-items-center justify-content-around mx-5">
-            <div>
-                <img src="/img/image_box_2-234x231.png" alt="">
-            </div>
-            <div class="ms-3">
-                <h3>Access For Business</h3>
-                <p>Get unlimited access to 2,500 of top courses for your team.</p>
-                <button>DOING BUSINESS</button>
+            <div class="p-3  col-6">
+                <div class="border d-flex align-items-center justify-content-around p-3">
+                    <div>
+                        <img src="/img/image_box_2-234x231.png" alt="">
+                    </div>
+                    <div class="ms-3">
+                        <h3 class="my-font-size">Access For Business</h3>
+                        <p class="fs-5">Get unlimited access to 2,500 of top courses for your team.</p>
+                        <button>DOING BUSINESS</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'PartnerComponent',
-    }
+export default {
+    name: 'PartnerComponent',
+}
 </script>
 
 <style lang="scss" scoped>
-
+.my-font-size {
+    font-size: 36px !important;
+}
 </style>
--- src/components/ReadMore.vue
+++ src/components/ReadMore.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="d-flex justify-content-center my-bg-img align-items-center">
         <div class="my-margin">
-            <h2>
-                Limitless learning, more possibilities
+            <h2 class="">
+                Limitless learning,<br>more possibilities
             </h2>
-            <p>Online courses open the opportunity for learning to almost anyone, regardless of their scheduling commitments.</p>
-            <button>READ MORE</button>
+            <p>Online courses open the opportunity for learning to almost anyone,<br>regardless of their scheduling commitments.</p>
+            <div class="mt-4 py-4">
+                <button class="btn position-relative">
+                    <span>READ MORE</span>
+                    <div></div>
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -28,4 +33,48 @@
         margin-right: 500px;
         margin-bottom: 200px;
     }
+
+    h2{
+        font-size: 50px;
+        font-weight: 500;    
+    }
+    p{
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    button{
+        background-color: #FF4450;
+        color: white;
+        padding: 20px;
+        padding-left: 40px;
+        padding-right: 40px;
+        border-radius: 35px;
+        overflow: hidden;
+
+        >span{
+            z-index: 200;
+            position: relative;
+        }
+
+        >div{
+            height: 100%;
+            width: 0px;
+            transition: all 0.5s ease;
+            position: absolute;
+            left: 0;
+            top: 0;
+            z-index: 1;
+            background-color: rgba($color: #000000, $alpha: 0.2);
+        }
+
+        &:hover{
+            background-color: #FF4450;
+            color: white;
+            >div{
+                width: 100%;
+            }
+        }
+    }
+
 </style>
