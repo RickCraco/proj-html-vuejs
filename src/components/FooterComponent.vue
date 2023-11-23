80ce6a5cff0f41b9142392a75e47bf56da4d1287
RickCraco <riccardocracolici43@gmail.com>
2023-11-23 10:56:50 +0100

    creato ed importato componente footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="my-bg-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <h4>ABOUT</h4>
+                </div>
+                <div class="col-3">
+                    <h4>CONTACT</h4>
+                </div>
+                <div class="col-3">
+                    <h4>SOCIAL NETWORK</h4>
+                </div>
+                <div class="col-3">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+    .my-bg-footer{
+        background-color: #273044;
+    }
+</style>
